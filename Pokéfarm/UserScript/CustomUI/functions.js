3c44a51697d869f170826abbf829d3cd1ca1f25e
Takamorii <46329926+Takamorii@users.noreply.github.com>
2020-03-05 15:38:03 -0800

    Update functions.js

--- Pokéfarm/UserScript/CustomUI/functions.js
+++ Pokéfarm/UserScript/CustomUI/functions.js
@@ -1,19 +1,21 @@
 function getPFQPage(conditional) {
-  var url = window.location.href, start = url.indexOf('.com/') + 5, page;
-  switch (conditional === undefined) {
-    case true:
-	    switch (url.indexOf('/', start)) {
-	      case -1:
-	  return url.substring(start, url.length);
-	 default:
-	  return url.substring(start, url.indexOf('/', start));
-	} break;
-   case false:
-	switch (url.indexOf('/', start)) {
-	 case -1:
-	  return (url.substring(start, url.length) === conditional);
-	 default:
-	  return (url.substring(start, url.indexOf('/', start)) === conditional);
-	} break;
- }
+	var url = window.location.href, start = url.indexOf('.com/') + 5, page;
+		switch (conditional === undefined) {
+			case true:
+				switch (url.indexOf('/', start)) {
+					case -1:
+						return url.substring(start, url.length);
+					default:
+						return url.substring(start, url.indexOf('/', start));
+				}
+				break;
+			case false:
+				switch (url.indexOf('/', start)) {
+					case -1:
+						return (url.substring(start, url.length) === conditional);
+					default:
+						return (url.substring(start, url.indexOf('/', start)) === conditional);
+				}
+				break;
+		}
 }
