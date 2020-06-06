6cfaa5bf6cfa328d6247f78933e671b83d0b8d20
Takamorii <46329926+Takamorii@users.noreply.github.com>
2020-06-05 17:20:59 -0700

    Update Functions.js

--- Pokéfarm/UserScript/MassDisplay/Functions.js
+++ Pokéfarm/UserScript/MassDisplay/Functions.js
@@ -1,32 +1,32 @@
 function Dialog(title, element) {
-  Dialog.disableScroll();
-  var d = this;
-  var a = $('<div></div>');
-  a.addClass('dialog');
-  var b = $('<div></div>');
-  b.appendTo(a);
-  var c = $('<div></div>');
-  c.appendTo(b);
-  b = $('<div></div>');
-  b.appendTo(c);
-  $('<h3></h3>').text(title).appendTo(b);
-  var e = $('<div></div>');
-  e.html(element).appendTo(b);
+	Dialog.disableScroll();
+	var d = this;
+	var a = $('<div></div>');
+	a.addClass('dialog');
+	var b = $('<div></div>');
+	b.appendTo(a);
+	var c = $('<div></div>');
+	c.appendTo(b);
+	b = $('<div></div>');
+	b.appendTo(c);
+	$('<h3></h3>').text(title).appendTo(b);
+	var e = $('<div></div>');
+	e.html(element).appendTo(b);
 
-  var f = $('<button></button>').attr('type', 'button');
-  f.text('Cancel');
-  f.click(function() { $('.dialog').remove(); Dialog.enableScroll(); });
-  f.css({'float': 'right', 'margin': '0 0 0 8px'});
-  f.attr('data-cancelbutton', '1');
-  f.appendTo(e);
+	var f = $('<button></button>').attr('type', 'button');
+	f.text('Cancel');
+	f.click(function() { $('.dialog').remove(); Dialog.enableScroll(); });
+	f.css({'float': 'right', 'margin': '0 0 0 8px'});
+	f.attr('data-cancelbutton', '1');
+	f.appendTo(e);
 
-  d.dialog = a;
-  a.appendTo(document.body);
+	d.dialog = a;
+	a.appendTo(document.body);
 }
 
 Dialog.disableScroll = function() {
-  var d = $("#core");
-  $(".dialog").css("position", "fixed");
+	var d = $("#core");
+	$(".dialog").css("position", "fixed");
 	if (!d.hasClass("scrolllock")) {
 		var a = Math.min(d[0].scrollHeight - window.innerHeight, document.documentElement.scrollTop || document.body.scrollTop);
 		d.addClass("scrolllock");
