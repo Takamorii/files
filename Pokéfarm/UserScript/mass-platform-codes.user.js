ef1987098aba367e2651cfe8684509bfda44b425
Takamorii <46329926+Takamorii@users.noreply.github.com>
2020-06-15 15:53:49 -0700

    Update mass-platform-codes.user.js

--- Pokéfarm/UserScript/mass-platform-codes.user.js
+++ Pokéfarm/UserScript/mass-platform-codes.user.js
@@ -22,46 +22,92 @@ waitForKeyElements('[data-menu="bulkmove"]', activateUserScript);
 
 function activateUserScript() {
  var private = $('#field_field[data-mode="private"]');
+ var codestuffs = $('<div id="message"></div><button id="reg">Regular</button><button id="shi">Shiny</button><button id="alb">Albino</button><button id="mel">Melanistic</button><button id="del">Delta</button>');
+ GM_addStyle('#message { min-width: 600px; min-height: 24px; padding-top: 2px; }');
  if(private) {
+  var data = getMonData();
   var button = $('<label hr-data-menu="bulkdisplay"></label').text('Mass Display Codes');
-  button.click(function() { copyCodes(); });
+  button.click(function() {
+   Dialog('Mass Display Codes', codestuffs)
+   $('#message').text('Click on a button to copy the corresponding platform codes to the clipboard!');
+   $('#reg').click(function() { copyCodes('regular', data['regular']) } );
+   $('#shi').click(function() { copyCodes('shiny', data['shiny']) } );
+   $('#alb').click(function() { copyCodes('albino', data['albino']) } );
+   $('#mel').click(function() { copyCodes('melanistic', data['melan']) } );
+   $('#del').click(function() { copyCodes('delta', data['delta']) } );
+  });
   private.find('[data-menu="bulkmove"]').before(button);
  } else {
   return;
  }
 
- function copyCodes() {
-  if($('.copied').length) {
-   return;
+ function copyCodes(message, data) {
+  if(data.length === 0) {
+   $('#message').text('There are no ' + message + ' Pokémon to display!');
   } else {
+   var selection = '';
+   for(var i = 0; i < data.length; i++) {
+	selection += '[url=https://pfq.link/?' + data[i] + '][img]https://pfq.link/?' + data[i] + '=platform.png[/img][/url]';
+   }
+   $('#message').text('Copied the platforms of all ' + message + ' Pokémon to the clipboard!');
    var select = document.createElement('input');
    select.setAttribute('type', 'text');
    select.setAttribute('id', 'select');
-   select.setAttribute('value', getMonData());
-   console.log(select);
+   select.setAttribute('value', selection);
    $('body').append(select);
    select.select();
    document.execCommand("copy");
    select.remove();
-   resetText(button, $('<span class="copied">Copied to Clipboard!</span>'), 'Mass Display Codes');
   }
+ }
+
+ function addButtonFunc(selector, func) {
+  console.log(func);
+  $(selector).click(function() { func } );
  }
 
  function getMonData() {
-  var mons = private.find('.fieldmon');
-  var code = '';
-  for(var i = 0; i < mons.length; i++) {
-   var id = mons.eq(i).attr('data-id');
-   code += '[url=https://pfq.link/?' + id + '][img]https://pfq.link/?' + id + '=platform.png[/img][/url]';
+  var mondata = new Map();
+  var regular = [], shiny = [], albino = [], melan = [], delta = [];
+  var fielddata = private.find('.fieldmontip');
+  for(var i = 0; i < fielddata.length; i++) {
+   var sel = fielddata[i];
+   if($(sel).find('[title="[SHINY]"]')) {
+	var mon = $(sel).find('[title="[SHINY]"]').closest('.tooltip_content').prev().attr('data-id');
+	if(mon != undefined) { shiny.push(mon); continue; }
+	else {
+	 if($(sel).find('[title="[ALBINO]"]')) {
+	  var mon = $(sel).find('[title="[ALBINO]"]').closest('.tooltip_content').prev().attr('data-id');
+	  if(mon != undefined) { albino.push(mon); continue; }
+	  else {
+	   if($(sel).find('[title="[MELANISTIC]"]')) {
+		var mon = $(sel).find('[title="[MELANISTIC]"]').closest('.tooltip_content').prev().attr('data-id');
+		if(mon != undefined) { melan.push(mon); continue; }
+		else {
+		 if($(sel).find('[title="[DELTA]"]')) {
+		  var mon = $(sel).find('[title="[DELTA]"]').closest('.tooltip_content').prev().attr('data-id');
+		  if(mon != undefined) { shiny.push(mon); continue; }
+		  else {
+		   regular.push(mon);
+		  }
+		  var sel = null;
+		 }
+		}
+		var sel = null;
+	   }
+	  }
+	  var sel = null;
+	 }
+	}
+	var sel = null;
+   }
   }
-  return code;
- }
+  mondata['regular'] = regular;
+  mondata['shiny'] = shiny;
+  mondata['albino'] = albino;
+  mondata['melan'] = melan;
+  mondata['delta'] = delta;
 
- function resetText(parent, span, text) {
-  button.text('').append(span);
-  span.fadeOut(3000);
-  setTimeout(function() {
-   parent.text(text);
-  }, 3000);
+  return mondata;
  }
 };
